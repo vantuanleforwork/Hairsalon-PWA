8e67acdf6ff7fe70acf2b343451fb2a7e001fe67
vantuanle <vantuanleforwork@gmail.com>
2025-09-07 15:29:23 +0700

    init

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const APP_CONFIG = {
     GOOGLE_CLIENT_ID: '36454863313-tlsos46mj2a63sa6k4hjralerarugtku.apps.googleusercontent.com',
     
     // Google Apps Script Web App URL (sau khi deploy)
-    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbwSXlMQ6QZlpLodC_fjsxewhwpShOj0zbUm37cLCwJTrcxikl1S6ka_u93qDrn3I-Wz/exec',
+    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbzEZneloVbMU027zmziwnr0TCDj5hkZppFHaUoORyPybQN6IO8xCakqrhzue74jfHiE/exec',
     
     // Danh sách email được phép truy cập (whitelist)
     ALLOWED_EMAILS: [
